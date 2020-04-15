f2eb45ca80dbf52950d5fcece264c4cac07da807
paweemew <pawee.tanti@gmail.com>
2020-04-15 16:00:35 +0700

    add pan function

---
+++ Assets/Scripts/panScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panScript : MonoBehaviour
+{
+    public bool panOn = true;
+    private float panTime = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cooking();
+        CheckPan();
+    }
+
+    void Cooking() {
+        if (steakScript.isCooking == true)
+        {
+            panTime = panTime + Time.deltaTime;
+        }
+        else if (steakScript.isCooking == false && panTime >= 0) {
+            panTime = panTime + Time.deltaTime;
+        }
+    }
+
+    void CheckPan() {
+
+        if (panTime > 10)
+        {
+            steakScript.state = 2;
+        }
+        else if (panTime > 5)
+        {
+            steakScript.state = 1;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/steakScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class steakScript : MonoBehaviour
+{
+    public static int state = 0;
+    public static bool isCooking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StateLog();
+    }
+
+    void StateLog() {
+
+        if (state == 1)
+        {
+            Debug.Log("cooked");
+        }
+        else if (state == 2) {
+            Debug.Log("overcooked");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    { 
+        if (collision.gameObject.tag == "pan") {
+           isCooking = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "pan")
+        {
+
+            isCooking = false;
+        }
+    }
+}
